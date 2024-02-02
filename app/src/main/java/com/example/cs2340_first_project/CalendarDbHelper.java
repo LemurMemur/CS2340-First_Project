a5bb1a000738308084ba787231e8149c944a40d4
LemurMemur <37851353+LemurMemur@users.noreply.github.com>
2024-02-01 22:01:01 -0500

    Added event ArrayList and helper methods to CalendarDbHelper

--- app/src/main/java/com/example/cs2340_first_project/CalendarDbHelper.java
+++ app/src/main/java/com/example/cs2340_first_project/CalendarDbHelper.java
@@ -1,5 +1,22 @@
 package com.example.cs2340_first_project;
 
+import java.util.ArrayList;
+
 public class CalendarDbHelper {
+    private ArrayList<Event> events;
+
+    public CalendarDbHelper() {
+        events = new ArrayList<Event>();
+    }
+
+    public void addEvent(Event newEvent) {
+        events.add(newEvent);
+    }
+    public void removeEvent(Event newEvent) {
+        events.remove(newEvent);
+    }
+    public ArrayList<Event> getEventList() {
+        return events;
+    }
 
 }
