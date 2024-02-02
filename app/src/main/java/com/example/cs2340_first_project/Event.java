858656b211a82d1080f1cdd0da84ddfa33cf2554
LemurMemur <37851353+LemurMemur@users.noreply.github.com>
2024-02-01 22:06:00 -0500

    Added instructor attribute to Event class

--- app/src/main/java/com/example/cs2340_first_project/Event.java
+++ app/src/main/java/com/example/cs2340_first_project/Event.java
@@ -3,22 +3,27 @@ package com.example.cs2340_first_project;
 public class Event {
     private String title;
     private String location;
+    private String instructor;
 
     public Event() {
         this.title = "";
         this.location = "";
+        this.instructor = "";
     }
 
-    public Event(String title, String location) {
+    public Event(String title, String location, String instructor) {
         this.title = title;
         this.location = location;
+        this.instructor = instructor;
     }
 
     public String getTitle() {
         return this.title;
     }
-
     public String getLocation() {
         return this.location;
+    }
+    public String getInstructor() {
+        return this.instructor;
     }
 }
